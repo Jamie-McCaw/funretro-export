e9b98e4088879a990c4e0b20a26b90ee85ec0b94
Jamie McCaw <psyral@gmail.com>
2021-01-16 10:25:26 -0500

    Adds default and choosing export type

--- index.js
+++ index.js
@@ -2,64 +2,81 @@ const fs = require('fs');
 const path = require('path');
 const { chromium } = require('playwright');
 
-const [url, file] = process.argv.slice(2);
+const [url, file, exportType] = process.argv.slice(2);
 
 if (!url) {
-  throw 'Please provide a URL as the first argument.';
+    throw 'Please provide a URL as the first argument.';
 }
 
 async function run() {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
 
-  await page.goto(url);
-  await page.waitForSelector('.message-list');
+    await page.goto(url);
+    await page.waitForSelector('.message-list');
 
-  const boardTitle = await page.$eval('#board-name', (node) => node.innerText.trim());
+    const boardTitle = await page.$eval('#board-name', (node) => node.innerText.trim());
 
-  if (!boardTitle) {
-    throw 'Board title does not exist. Please check if provided URL is correct.'
-  }
-
-  let parsedText = boardTitle + '\n\n';
-
-  const columns = await page.$$('.message-list');
-
-  for (let i = 0; i < columns.length; i++) {
-    const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
-
-    const messages = await columns[i].$$('.message-main');
-    if (messages.length) {
-      parsedText += columnTitle + '\n';
-    }
-    for (let i = 0; i < messages.length; i++) {
-      const messageText = await messages[i].$eval('.message-body .text', (node) => node.innerText.trim());
-      const votes = await messages[i].$eval('.votes .vote-area span.show-vote-count', (node) => node.innerText.trim());
-      parsedText += `- ${messageText} (${votes})` + '\n';
+    if (!boardTitle) {
+        throw 'Board title does not exist. Please check if provided URL is correct.'
     }
 
-    if (messages.length) {
-      parsedText += '\n';
-    }
-  }
+    let parsedText = boardTitle + '\n\n';
 
-  return parsedText;
+    const columns = await page.$$('.message-list');
+
+    for (let i = 0; i < columns.length; i++) {
+        const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
+
+        const messages = await columns[i].$$('.message-main');
+        if (messages.length) {
+            parsedText += columnTitle + '\n';
+        }
+        for (let i = 0; i < messages.length; i++) {
+            const messageText = await messages[i].$eval('.message-body .text', (node) => node.innerText.trim());
+            const votes = await messages[i].$eval('.votes .vote-area span.show-vote-count', (node) => node.innerText.trim());
+            parsedText += `- ${messageText} (${votes})` + '\n';
+        }
+
+        if (messages.length) {
+            parsedText += '\n';
+        }
+    }
+    return parsedText;
 }
 
-function writeToFile(filePath, data) {
-  const resolvedPath = path.resolve(filePath || `../${data.split('\n')[0].replace('/', '')}.txt`);
-  fs.writeFile(resolvedPath, data, (error) => {
-    if (error) {
-      throw error;
-    } else {
-      console.log(`Successfully written to file at: ${resolvedPath}`);
+function writeToFile(filePath, data, exportType) {
+    const resolvedPath = path.resolve(filePath || `../${data.split('\n')[0].replace('/', '')}.txt`);
+    switch (exportType) {
+        case "txt":
+            writeTxt(resolvedPath, data)
+            break;
+        case "csv":
+            writeCsv(resolvedPath, data)
+            break;
+        default:
+            writeTxt(resolvedPath, data)
     }
-    process.exit();
-  });
+}
+
+function writeTxt(resolvedPath, data) {
+    fs.writeFile(resolvedPath, data, (error) => {
+        if (error) {
+            throw error;
+        } else {
+            console.log(`Successfully written to file at: ${resolvedPath}`);
+        }
+        process.exit()
+    });
+}
+
+function writeCsv(resolvedPath, data) {
+    console.log(`You chose csv`);
+    process.exit()
 }
 
 function handleError(error) {
-  console.error(error);
+    console.error(error);
 }
 
-run().then((data) => writeToFile(file, data)).catch(handleError);
+run().then((data) => writeToFile(file, data, exportType)).catch(handleError);
